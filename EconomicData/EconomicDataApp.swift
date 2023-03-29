546f022070094c8db4d4f9b4bd75fab2eae3c9bf
Michael Magen <michael.magen101@gmail.com>
2023-03-29 10:35:14 -0700

    Initial Commit

---
+++ EconomicData/EconomicDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  EconomicDataApp.swift
+//  EconomicData
+//
+//  Created by Michael Magen on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct EconomicDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
